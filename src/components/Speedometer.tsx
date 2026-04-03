4f414cca08fcb638c36bc0713ec6c6446eaa624a
weididev <snehamoydutta11@gmail.com>
2026-04-03 19:16:16 +0530

    Update Speedometer.tsx

--- src/components/Speedometer.tsx
+++ src/components/Speedometer.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { cn } from '../lib/utils';
 import { Gauge, Play, Square, MapPin, Clock } from 'lucide-react';
 import { Bike, ActiveTrip } from '../types';
