a0a2eaf6cf342e259ac09d13ed7377934f2cf3cf
weididev <snehamoydutta11@gmail.com>
2026-04-03 18:26:07 +0530

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.motomate.app',
+  appName: 'MotoMate',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
