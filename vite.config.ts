4225115b962c013277a08c98ae289930d6d5f1fd
weididev <snehamoydutta11@gmail.com>
2026-04-03 18:55:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// ESM environment mein __dirname define karne ka sahi tarika
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  base: './', // Yeh relative path ke liye sabse zaroori hai
+  base: './', // Android APK ke liye zaroori
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // Yeh alias `@/` ko project ke root folder se map karega
+      '@': path.resolve(__dirname, './')
+    }
+  },
+  build: {
+    target: 'es2020',
+    chunkSizeWarningLimit: 2000
+  }
 })
